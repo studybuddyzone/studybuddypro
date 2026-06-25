7dcc9921989d1bdbbb13c4827bb7b16194e6aebf
studybuddyzone <websiteowner.in@gmail.com>
2026-06-25 21:21:01 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
   });
 
   const options = {
-    hostname: "sandbox.cashfree.com",
+    hostname: "api.cashfree.com",
     path: "/pg/orders",
     method: "POST",
     headers: {
